c30cc2b1b74634347c244866379c427864551de6
Bigtruck2 <bigburrito363@gmail.com>
2024-09-23 11:41:49 -0400

    Simplify

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -2,36 +2,19 @@ public class Scoreboard {
     private final String teamOneName;
     private final String teamTwoName;
     private boolean teamOneActive = true;
-    private int scoreOne = 0;
-    private int scoreTwo = 0;
-
-
+    int[] scores = {0,0};
     public Scoreboard(String teamOneName, String teamTwoName){
         this.teamOneName = teamOneName;
         this.teamTwoName = teamTwoName;
     }
-    /*During a turn, a team makes one or
-more plays. Each play can score one or more points and the team’s turn continues, or the play can fail, in
-which case no points are scored and the team’s turn ends. The Scoreboard class, which you will write, is
-used to keep track of the score in a game.
-     */
     public void recordPlay(int pointsScored){
         if(pointsScored == 0){
-            teamOneActive =!teamOneActive;
+            teamOneActive =! teamOneActive;
             return;
         }
-        if(teamOneActive){
-            scoreOne+=pointsScored;
-        }else {
-            scoreTwo+=pointsScored;
-        }
+        scores[teamOneActive ? 0 :1] += pointsScored;
     }
-
     public String getScore() {
-        if(teamOneActive){
-            return scoreOne+"-"+scoreTwo+"-"+teamOneName;
-        }else {
-            return scoreOne+"-"+scoreTwo+"-"+teamTwoName;
-        }
+        return scores[0]+"-"+scores[1]+ (teamOneActive ? teamOneName : teamTwoName);
     }
 }

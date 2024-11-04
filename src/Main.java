94c4961352ab48a420865777d7c8cd763d7eee39
Bigtruck2 <bigburrito363@gmail.com>
2024-11-04 18:05:32 -0500

    make things more clear

--- src/Main.java
+++ src/Main.java
@@ -3,56 +3,40 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         File file = new File("src/Scoreboard.txt");
-        int[] score;
-        HashMap<String,Integer> winingScores = new HashMap<>();
-
-        System.out.println(file);
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(file);
-        }catch (FileNotFoundException ignored){
-            System.err.println("wrong file");
-        }
+        HashMap<String,Integer> teams = new HashMap<>();
+        Scanner scanner = new Scanner(file);
         Scoreboard game = null;
         do {
-            assert scanner != null;
             if(!scanner.hasNextInt()){
                 if(game!=null) {
-                    score = game.getScores();
-                    if(score[0] != score[1]) {
-                        if (score[0] > score[1]) {
-                            if (winingScores.containsKey(game.getTeamOneName()))
-                                winingScores.put(game.getTeamOneName(), winingScores.get(game.getTeamOneName()) + 1);
-                            winingScores.putIfAbsent(game.getTeamOneName(), 1);
-                        } else {
-                            if (winingScores.containsKey(game.getTeamTwoName()))
-                                winingScores.put(game.getTeamTwoName(), winingScores.get(game.getTeamTwoName()) + 1);
-                            winingScores.putIfAbsent(game.getTeamTwoName(), 1);
-                        }
-                    }
+                   incrementWinner(game,teams);
                 }
                 game = new Scoreboard(scanner.next(), scanner.next());
-
             }else {
                 assert game != null;
                 game.recordPlay(scanner.nextInt());
             }
         } while (scanner.hasNext());
-        score = game.getScores();
-        if(score[0] != score[1]) {
-            if (score[0] > score[1]) {
-                if (winingScores.containsKey(game.getTeamOneName()))
-                    winingScores.put(game.getTeamOneName(), winingScores.get(game.getTeamOneName()));
-                winingScores.putIfAbsent(game.getTeamOneName(), 1);
-            } else {
-                if (winingScores.containsKey(game.getTeamTwoName()))
-                    winingScores.put(game.getTeamTwoName(), winingScores.get(game.getTeamTwoName()));
-                winingScores.putIfAbsent(game.getTeamTwoName(), 1);
-            }
-        }
-        System.out.println(winingScores);
-        winingScores.values().stream().sorted(Comparator.reverseOrder()).forEach((i)->System.out.printf("Wins: %d Team: %s\n",i,winingScores.entrySet().stream().filter((entry)->entry.getValue().equals(i)).findFirst().orElse(null)));
+        incrementWinner(game,teams);
+        System.out.println(teams);
+        teams.values().stream().sorted(Comparator.reverseOrder())
+        .forEach((i)->System.out.printf("Wins: %d Team: %s\n",i, Objects.requireNonNull(teams.entrySet().stream()
+        .filter((entry) -> entry.getValue()==i).findFirst().orElse(null)).getKey()));
     }
+public static void incrementWinner( Scoreboard game, HashMap<String,Integer> teams){
+    int[] score = game.getScores();
+    if(score[0] != score[1]) {
+        if (score[0] > score[1]) {
+            if (teams.containsKey(game.getTeamOneName()))
+                teams.put(game.getTeamOneName(), teams.get(game.getTeamOneName()) + 1);
+            teams.putIfAbsent(game.getTeamOneName(), 1);
+        } else {
+            if (teams.containsKey(game.getTeamTwoName()))
+                teams.put(game.getTeamTwoName(), teams.get(game.getTeamTwoName()) + 1);
+            teams.putIfAbsent(game.getTeamTwoName(), 1);
+        }
+    }
+}
 }
--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -2,7 +2,6 @@ public class Scoreboard {
     private final String teamOneName,teamTwoName;
     private boolean teamOneActive = true;
     private final int[] scores = {0,0};
-
     public Scoreboard(String teamOneName, String teamTwoName){
         this.teamOneName = teamOneName;
         this.teamTwoName = teamTwoName;
@@ -14,19 +13,15 @@ public class Scoreboard {
         }
         scores[teamOneActive ? 0 : 1] += pointsScored;
     }
-
     public int[] getScores() {
         return scores;
     }
-
     public String getTeamOneName() {
         return teamOneName;
     }
-
     public String getTeamTwoName() {
         return teamTwoName;
     }
-
     public String getScore() {
         return scores[0]+"-"+scores[1]+"-"+(teamOneActive ? teamOneName : teamTwoName);
     }
